e5bebd6a57be742f2c73b1ab0aa6c5d2317eb0f4
ISCAndres <spartanlied@hotmail.com>
2021-08-03 19:56:35 -0500

    primerCommit

---
+++ form.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title> Sistema tienda </title>
+	</head>
+	<body background= "2.jpg">
+	<center><font face="algerian" color="black" size="96">
+		Bienvenidos <br>
+		Menu de Sistema de Tienda</font>
+		<br><br>
+		<Table border=1>
+		<tr > 
+			<td><a href="proveedor.html"> <img src="logo_pro.png" height="70" width="70"> <br> Menu Proveeores </a>  </td>
+			<td><a href="tienda.html"> <img src="logo_tienda.png" height="70" width="70"> <br> Menu Tienda</a> </td>
+		</tr>
+		<tr>
+			<td><a href="articulo.html"> <img src="logo_arti.png" height="70" width="70"> <br> Menu Articulo</a> <br><br></td>
+			<td><a href="empleado.html"> <img src="logo_emple.png" height="70" width="70"> <br> Menu Empleado</a> <br><br></td>
+		</tr>
+		<tr>
+			<td><a href="requisicion.html"> <img src="logo_requi.png" height="70" width="70"> <br> Menu Requisicion</a> <br><br></td>
+			<td><a href="factura.html"> <img src="logo_factura.png" height="70" width="70"> <br> Menu Factura</a> <br><br></td>	
+		</tr>
+		</table>
+	</body>
+</html>
---
+++ inicio.php
@@ -0,0 +1,39 @@
+<head>
+
+<body bgcolor="#7fffd4">
+
+<br>
+<center>
+
+<table border="5">
+<tr>
+
+<font size="15">AFD</font>
+<br>
+<br>
+<br>
+<br>
+<br>
+<td><a href="https://www.uaeh.edu.mx/docencia/P_Presentaciones/huejutla/sistemas/lenguajes_automatas/afd.pdf">Autómata Finito Determinista (Descripción)</href></td>
+<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+<td><a href="https://www.youtube.com/watch?v=P0AxQvJcN2Q">Autómata Finito Determinista (Vídeo)</href></td>
+<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+<td><a href="mailto:ja.balandran.v@edu.utc.mx">Contacto</href></td>
+</td>
+</table>
+</center>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<Center><IMG SRC="AFN.png" width=354 height=207></Center>
+<br>
+<br>
+<br>
+<br>
+
+<Center><FORM METHOD="LINK" ACTION="AFND.html"><INPUT TYPE="submit" VALUE="AFND"></Center>
+</body>
+</html>
